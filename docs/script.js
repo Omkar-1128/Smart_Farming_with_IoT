0b6784db843187960f5e55952563fb4e73bd79af
yuvrajwable-7 <yuvrajwable305@gmail.com>
2025-10-01 00:14:45 +0530

    new all change

--- docs/script.js
+++ docs/script.js
@@ -1,9 +1,14 @@
 // script.js
 
 window.addEventListener("DOMContentLoaded", () => {
+  const h1 = document.querySelector("h1");
   const h2 = document.querySelector("h2");
 
-  // Fade-in animation after 1 second
+  // Show h1 and h2 with animations
+  setTimeout(() => {
+    h1.style.opacity = 1;
+  }, 200);
+
   setTimeout(() => {
     h2.classList.add("fade-in");
   }, 1000);
